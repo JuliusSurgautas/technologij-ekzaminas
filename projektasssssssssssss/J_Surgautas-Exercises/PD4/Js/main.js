a902820a3688cdc65f8d8e9ee5f66033d1af4f44
J Surgautas-Exercises <144884734+JuliusSurgautas@users.noreply.github.com>
2023-11-29 14:07:52 +0200

    20231129 dėl visa ko kopija

---
+++ projektasssssssssssss/J_Surgautas-Exercises/PD4/Js/main.js
@@ -0,0 +1,18 @@
+let petName;
+let petAge;
+
+
+petName='Jon';
+petAge=40;
+
+
+console.log(petName);
+comsole.log(petAge);
+
+
+petName='Binky';
+petAge=14;
+
+
+const buildingAdress='Trinapolio g.2';
+console.log(buildingAdress);
