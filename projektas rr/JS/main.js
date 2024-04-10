0010699b7573fa8c56cda1ed75f1ef33efe5f123
J Surgautas-Exercises <144884734+JuliusSurgautas@users.noreply.github.com>
2024-04-10 13:37:06 +0300

    Bandymas projekto

---
+++ projektas rr/JS/main.js
@@ -0,0 +1,11 @@
+
+// išvedamas mygtukas
+let mybutton = document.getElementById("myBtn");
+
+
+
+// paspaudus mygtuką kele i viršu
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
